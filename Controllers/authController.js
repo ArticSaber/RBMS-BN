4136090eb9a53dc97d8713625867bbe8d22fdd08
ArticSaber <kaarthiknirmala@gmail.com>
2023-11-03 19:15:03 +0530

    login validation

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -9,6 +9,8 @@ const authlogin = async (req, res, next) => {
       throw new badRequest("Email and Password is required");
     const User = await authSchema.findOne({ email: req.body.email });
     if (!User) throw new badRequest("User not found");
+    const isActive = User.active;
+    if (!isActive) throw new badRequest("User is not active");
     const isMatch = await comparePassword(req.body.password, User.password);
     if (!isMatch) throw new badRequest("Password is not correct");
     const token = jwtGenrator({ payload: { id: User._id, role: User.role } });
